12042f4452a783f61b0e256eb336e6e6754bed4e
rubenrubiom <rubensipala@gmail.com>
2022-08-06 21:30:10 +0200

    Input parameters

--- scrapped_webs/src/sephora.rs
+++ scrapped_webs/src/sephora.rs
@@ -13,6 +13,8 @@ use crate::{
 use scraper::ElementRef;
 use scraper::Html;
 
+use ansi_term::Colour::RGB;
+
 /// Module for sephora.es
 pub mod sephora_spain {
     use super::*;
@@ -101,10 +103,13 @@ pub mod sephora_spain {
                 if similarity >= self.config.min_similarity() {
                     urls.push(url.to_string());
                 } else {
-                    eprintln!(
-                        "Discarding: {} with {:.2}% of similarity because it is below the minimum",
-                        full_name,
-                        similarity * 100.0
+                    println!(
+                        "{}",
+                        RGB(255, 121, 0).normal().paint(format!(
+                            "Discarding [{:.2}%]: {}",
+                            similarity * 100.0,
+                            full_name
+                        ))
                     );
                 }
             }
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,31 @@
 use std::collections::HashMap;
-use std::env;
 
 use scrapped_webs::configuration::Configuration;
 use scrapped_webs::product::Product;
 use scrapped_webs::scrappable::Scrappable;
 use scrapped_webs::sephora::sephora_spain::SephoraSpain;
 
+mod parameters;
+use clap::Parser;
+use parameters::Args;
+
 fn main() {
-    let conf: Configuration = Configuration::new(0.0, usize::MAX);
-    let sephora_spain = SephoraSpain::new(&conf);
-    let args: Vec<String> = env::args().collect();
+    let args = Args::parse();
 
-    if args.len() == 2 {
-        let mut products_by_shop = HashMap::<&str, Vec<Product>>::new();
-        match sephora_spain.look_for_products(args.get(1).unwrap().to_string()) {
-            Ok(products) => {
-                products_by_shop.insert("SephoraSpain", products);
-            }
-            Err(search_error) => {
-                eprintln!("{search_error}");
-            }
-        };
+    println!("{}", args.product);
 
-        println!("{:#?}", products_by_shop);
-        println!("Makeup comparator!");
-    } else {
-        eprintln!("Provide a product to search as an input parameter.");
-    }
+    let conf: Configuration = Configuration::new(args.min_similarity, args.max_results);
+
+    let mut products_by_shop = HashMap::<&str, Vec<Product>>::new();
+    match SephoraSpain::new(&conf).look_for_products(args.product) {
+        Ok(products) => {
+            products_by_shop.insert("SephoraSpain", products);
+        }
+        Err(search_error) => {
+            eprintln!("{search_error}");
+        }
+    };
+
+    println!("{:#?}", products_by_shop);
+    println!("Makeup comparator!");
 }
